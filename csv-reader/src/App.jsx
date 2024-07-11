c308997f3291367792688462a07a3cb8264ca887
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2024-07-11 11:41:01 +0530

    Final working version of the project

--- csv-reader/src/App.jsx
+++ csv-reader/src/App.jsx
@@ -31,11 +31,25 @@ function App() {
     return password;
   };
 
+  const downloadJSON = () => {
+    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.json';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
   return (
     <div className="App">
       <h1>CSV File Reader</h1>
       <input type="file" accept=".csv" onChange={handleFileUpload} />
       <Table data={data} />
+      {data.length > 0 && (
+        <button onClick={downloadJSON}>Download JSON</button>
+      )}
     </div>
   );
 }
